fe4013c7f6a222d64ed18f1e2aa00cb4d4b45609
Jeremy Ellington <jme@eminor.net>
2023-12-17 10:46:29 -0600

    Cleanup our drum_voice code such that it's clear, working example.

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # daisy
 Daisy microcontroller stuff
+
+`DaisySP` and `libDaisy` must be built before anything else.
+
+Submodule initialization:
+```
+git submodule add git@github.com:electro-smith/DaisySP
+git submodule add git@github.com:pichenettes/stmlib
+git submodule add git@github.com:electro-smith/libDaisy
+```
+
+
+
--- drum_voice/drum_voice.cpp
+++ drum_voice/drum_voice.cpp
@@ -1,3 +1,14 @@
+/**
+ *  A simple trigger-based drum voice.
+ *
+ * Gate 1: Trigger
+ *
+ * CV 1: Grit
+ * CV 2: FM envelope
+ * CV 5: Decay
+ * CV 6: FM envelope decay
+ */
+
 #include "daisy_patch_sm.h"
 #include "daisysp.h"
 
@@ -6,44 +17,27 @@ using namespace patch_sm;
 using namespace daisysp;
 
 SyntheticBassDrum bd;
-//Metro             tick;
-
 DaisyPatchSM hw;
 
 /**
- * Called back very frequently.
+ * Processes audio blocks.
+ *
+ * At 48Khz and a block size of 4, this is called at 12Khz, meaning that
+ * we have 83 ms to perform our computations.
  */
 void AudioCallback(AudioHandle::InputBuffer in,
                    AudioHandle::OutputBuffer out,
                    size_t size) {
   hw.ProcessAllControls();
-  // float distortion = hw.GetAdcValue(CV_1);
-  // float decay      = hw.GetAdcValue(CV_5);
-
   bool triggered = hw.gate_in_1.Trig();
 
   for(size_t i = 0; i < size; i++) {
-    // float t = tick.Process();
-    // bool triggered = hw.gate_in_1.State();
-    // float distortion = hw.GetAdcValue(CV_1);
-    // float decay = hw.GetAdcValue(CV_5);
-	// if (triggered) {
-	//   bd.SetAccent(0.9);
-	//   bd.SetDirtiness(distortion);
-	//   bd.SetDecay(decay);
-	// }
-    // if (triggered) {
-    //   bd.SetAccent(0.9);
-    //   bd.SetDirtiness(distortion);
-    //   bd.SetDecay(decay);
-    // }
     if (triggered) {
 	  bd.SetAccent(0.9);
       bd.SetDirtiness(hw.GetAdcValue(CV_1));
       bd.SetDecay(hw.GetAdcValue(CV_5));
       bd.SetFmEnvelopeAmount(hw.GetAdcValue(CV_2));
       bd.SetFmEnvelopeDecay(hw.GetAdcValue(CV_6));
-
       bd.Trig();
     }
     out[0][i] = out[1][i] = bd.Process();
@@ -52,10 +46,9 @@ void AudioCallback(AudioHandle::InputBuffer in,
   triggered = false;
 }
 
-int main(void)
-{
+int main(void) {
 	hw.Init();
-	hw.SetAudioBlockSize(4); // number of samples handled per callback
+	hw.SetAudioBlockSize(4);  // Number of samples handled per callback
 	hw.SetAudioSampleRate(SaiHandle::Config::SampleRate::SAI_48KHZ);
 
     bd.Init(48000);
@@ -63,10 +56,6 @@ int main(void)
     bd.SetDirtiness(.5f);
     bd.SetDecay(.1f);
     bd.SetFmEnvelopeAmount(.6f);
-
-    // tick.Init(2.f, 48000);
-
 	hw.StartAudio(AudioCallback);
-
   	while(1) {}
 }
